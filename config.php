94ee80eba0253559618f23d29b33f6409f145c33
SeregaShoman <NeshniiLovelas@yandex.ru>
2022-04-22 02:22:33 +0400

    Добавил конфиг и callback

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+  
+define('CLIENT_ID', 't12kPHMnStSAUGdPA1cD6g');
+define('CLIENT_SECRET', 'xlOlVKz9zgVACY7EfANariUBvWkXm8GV');
+define('REDIRECT_URI', 'https://68a6-46-0-208-56.eu.ngrok.io');
+
