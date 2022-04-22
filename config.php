a54061d918abb8f21b0357f7f83cc7428bb70869
SeregaShoman <NeshniiLovelas@yandex.ru>
2022-04-23 01:05:14 +0400

    добавил новый редирект и кнопку

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ require_once "class-db.php";
   
 define('CLIENT_ID', 't12kPHMnStSAUGdPA1cD6g');
 define('CLIENT_SECRET', 'xlOlVKz9zgVACY7EfANariUBvWkXm8GV');
-define('REDIRECT_URI', 'https://68a6-46-0-208-56.eu.ngrok.io');
+define('REDIRECT_URI', 'https://56a2-46-0-208-56.eu.ngrok.io');
 
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+
 require_once 'config.php';
+
+
 $url = "https://zoom.us/oauth/authorize?response_type=code&client_id=".CLIENT_ID."&redirect_uri=".REDIRECT_URI;
 ?>
 <center>
@@ -7,5 +10,14 @@ $url = "https://zoom.us/oauth/authorize?response_type=code&client_id=".CLIENT_ID
     <input type="date"  name="start_time" required placeholder="Дата" min="2022-01-01" max="2030-12-31"><br>                      
     <input type="text"  name="topic" required placeholder="Тема Встречи"><br>                      
     <input type="number"  name="duration"  required placeholder="Длительность"><br>                      
-    <input type="number"  name="password"  required placeholder="Пароль"><br>                      
+    <input type="number"  name="password"  required placeholder="Пароль"><br>         
+    <form method="POST">
+    <input type="submit" name="createZoomMeeting()" value="Создать встречу" />
+</form>   
+<?php
+    if( isset( $_POST['createZoomMeeting()'] ) )
+    {
+        createZoomMeeting();
+    }
+?>
 </center>
